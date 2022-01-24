a05d269e6396dbb41b1e2bdd11163659a86eb8d0
Kenneth Mercer <kmercer5439@gmail.com>
2022-01-24 15:39:25 -0600

    episode testing finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
 
 import Display from "./components/Display";
+import Episode from './components/Episode';
 
 import "./App.css";
+
+const providedExample = {
+  id: 1,
+  image: '',
+  name: 'This is a test name',
+  season: 1,
+  number: 1,
+  summary: 'This is a test summary',
+  runtime: 1
+}
 
 export default function App() {
   const displayFunc = (data)=> {
@@ -16,6 +27,7 @@ export default function App() {
       </nav>
       <div className="App">
         <Display displayFun={displayFunc}/>
+        <Episode episode={providedExample}/>
       </div>
     </div>
   );
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,35 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const providedExample = {
+    id: 1,
+    image: '',
+    name: 'This is a test name',
+    season: 1,
+    number: 1,
+    summary: 'This is a test summary',
+    runtime: 1
+  }
+test("renders without error", () => {
+    render(<Episode episode={[]}/>)
+});
 
-test("renders without error", () => {});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={providedExample}/>)
 
-test("renders the summary test passed as prop", ()=>{});
+    const summary = screen.queryByText(/This is a test summary/i);
 
-test("renders default image when image is not defined", ()=>{});
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeDefined();
+    expect(summary).not.toBeFalsy();
+
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={providedExample}/>)
+
+    const imgAlt = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+
+    expect(imgAlt).toBeInTheDocument();
+
+});

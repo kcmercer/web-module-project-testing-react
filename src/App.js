8de584d59e87d1e00a15230ee9d4006b37cb12c7
Kenneth Mercer <kmercer5439@gmail.com>
2022-01-24 17:57:00 -0600

    working on display mock data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, { useState } from "react";
 
 import Display from "./components/Display";
-import Episode from './components/Episode';
-import Show from './components/Show';
 
 import "./App.css";
-
-const providedExample = {
-  id: 1,
-  image: '',
-  name: 'This is a test name',
-  season: 1,
-  number: 1,
-  summary: 'This is a test summary',
-  runtime: 1
-}
-
-const exampleData = {
-  name: 'This is a test show',
-  summary: 'This is a test summary',
-  seasons: [
-      {id: 1, name: 'Season 1', episodes: []},
-      {id: 2, name: 'Season 2', episodes: []},
-      {id: 3, name: 'Season 3', episodes: []},
-  ]
-}
 
 export default function App() {
   const displayFunc = (data)=> {
@@ -37,7 +15,7 @@ export default function App() {
         <a className="navbar-brand" href="#">Integration Testing Challenge</a>
       </nav>
       <div className="App">
-        <Display displayFun={displayFunc}/>
+        <Display/>
       </div>
     </div>
   );
--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -2,20 +2,44 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 
 import Display from './../Display';
+import { fetchShow } from '../../api/fetchShow';
 
+// jest.mock('../../api/fetchShow');
 
 test('renders without errors with no props', ()=>{
     render(<Display />);
 });
 
 test('renders Show component when the button is clicked ', async ()=>{
+    fetchShow.mockResolvedValueOnce({
+        data: [{
+            name: 'This is a test show',
+            image:'',
+            summary: 'This is a test summary',
+            seasons: [
+                {id: 1, name: 'Season 1', episodes: [{
+                    id: 1,
+                    image: '',
+                    name: 'This is a test name',
+                    season: 1,
+                    number: 1,
+                    summary: 'This is a test summary',
+                    runtime: 1}]},
+                {id: 2, name: 'Season 2', episodes: []},
+                {id: 3, name: 'Season 3', episodes: []},
+            ]
+        }]
+    });
+
     render(<Display />);
 
     const button = screen.getByRole('button');
     userEvent.click(button);
 
+    screen.debug();
 
     const showData = await screen.findByTestId('show-container');
     expect(showData).toBeInTheDocument();
@@ -29,5 +53,4 @@ test('renders show season options matching your data when the button is clicked'
 
     const seasonOptions = await screen.findAllByTestId('season-option');
     expect(seasonOptions).toHaveLength(4);
-
 });
